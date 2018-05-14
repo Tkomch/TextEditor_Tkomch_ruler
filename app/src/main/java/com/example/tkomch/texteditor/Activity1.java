483320e16fe464ff1aaf718b1d6bf9d79f85c2f8
Tkomch <a1458675007@live.com>
2018-05-14 14:37:09 +0800

    AndroidStudio框架

---
+++ app/src/main/java/com/example/tkomch/texteditor/Activity1.java
@@ -0,0 +1,18 @@
+package com.example.tkomch.texteditor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity1 extends AppCompatActivity {
+
+    ListView lv = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+
+
+    }
+}
